798dde410f9726e8d21cae28483c483c01112e26
janmetten <janmetten@hoppinger.com>
2015-01-05 09:00:30 +0100

    Added customerlanguage parameter

--- app/models/spree/omnikassa_payment.rb
+++ app/models/spree/omnikassa_payment.rb
@@ -25,7 +25,8 @@ module Spree
         normalReturnUrl: normal_return_url,
         automaticResponseUrl: automatic_response_url,
         transactionReference: transaction_reference,
-        keyVersion: key_version }
+        keyVersion: key_version,
+        customerLanguage: current_language.nil? ? '' : current_language }
     end
 
     def money
@@ -76,6 +77,10 @@ module Spree
     def self.secret
       Spree::Config[:omnikassa_secret_key]
     end
+  end
+
+  def current_language
+      I18n.locale[0..1].upcase if ['CS', 'CY', 'DE', 'EN', 'ES', 'FR', 'NL', 'SK'].include?(I18n.locale[0..1].upcase)
   end
 
   class OmnikassaPayment < ActiveRecord::Base
