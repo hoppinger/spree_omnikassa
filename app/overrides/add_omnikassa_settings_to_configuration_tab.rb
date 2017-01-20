7fb28dda6ee45c3bb365e0ce15f101dbd1bd7e72
Jan Metten Van der Meer <janmetten@Jans-MacBook-Pro.local>
2017-01-20 15:11:28 +0100

    Fix configuration override

--- app/overrides/add_omnikassa_settings_to_configuration_tab.rb
+++ app/overrides/add_omnikassa_settings_to_configuration_tab.rb
@@ -1,8 +1,9 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-                     :name => "add_omnikassa_settings_to_configuration_menu",
-                     :insert_bottom => '[data-hook="admin_configurations_sidebar_menu"]',
-                     :text => '<%= configurations_sidebar_menu_item t(:omnikassa_settings), edit_admin_omnikassa_settings_path %>')
-
+Deface::Override.new(
+                       virtual_path: 'spree/admin/shared/sub_menu/_configuration',
+                       name: "add_omnikassa_settings_to_configuration_menu",
+                       insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
+                       text: '<%= configurations_sidebar_menu_item t(:omnikassa_settings), spree.edit_admin_omnikassa_settings_path %>')
+                     )
 # Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
 #                      :name => "add_omnikassa_settings_to_configuration_index",
 #                      :insert_bottom => '[data-hook="admin_configurations_menu"]',
@@ -10,4 +11,3 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
 #                                  <td><%= link_to t(:omnikassa_settings), :edit_admin_omnikassa_settings %></td>
 #                                  <td><%= t(:omnikassa_settings) %></td>
 #                                </tr>')
-
